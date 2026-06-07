fd6e7142dc552892aa9cac65c2e293605a92bb7f
Yeshwanth <yeshwanth@Prathushas-MacBook-Air.local>
2026-06-08 00:24:47 +0530

    feat(location): add Select Location & gate screens + route

---
+++ app/location/index.tsx
@@ -0,0 +1,7 @@
+// app/location/index.tsx
+
+import { LocationGateScreen } from '@/src/features/location/views/LocationGateScreen';
+
+export default function LocationRoute() {
+  return <LocationGateScreen />;
+}
